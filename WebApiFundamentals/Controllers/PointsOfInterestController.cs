ce76c2c97d44918960c3e82d4a6eb7bdc8bb8d05
Mateusz Wojnarowski <mateusz.wojnarowski@bighand.com>
2023-09-25 19:14:02 +0200

    Webapi fundamentals

---
+++ WebApiFundamentals/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiFundamentals.Models;
+
+namespace WebApiFundamentals.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poi = city.PointsOfInterest.SingleOrDefault(p => p.Id == pointOfInterestId);
+
+            if(poi == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(poi);
+        }
+    }
+}
