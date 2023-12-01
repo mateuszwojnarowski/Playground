862dd0318adbea9e8184198738319a78b6df3c68
Mateusz Wojnarowski <mateusz.wojnarowski@bighand.com>
2023-12-01 20:52:40 +0100

    .

--- WebApiFundamentals/Controllers/PointsOfInterestController.cs
+++ WebApiFundamentals/Controllers/PointsOfInterestController.cs
@@ -21,7 +21,7 @@ namespace WebApiFundamentals.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name ="GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
         {
             var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
@@ -39,6 +39,30 @@ namespace WebApiFundamentals.Controllers
             }
 
             return Ok(poi);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInerest(int cityId, PointOfInterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.Find(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // demo purposes - to be improved
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var poi = new PointOfInterestDto
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(poi);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, pointOfInterestId = poi.Id }, poi);
         }
     }
 }
---
+++ WebApiFundamentals/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiFundamentals.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
