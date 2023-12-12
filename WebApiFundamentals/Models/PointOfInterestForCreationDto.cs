32a8103181bec2ca144bd76a93132d85f7f7da54
Mateusz Wojnarowski <mateusz.wojnarowski@bighand.com>
2023-12-12 23:53:07 +0100

    Some changes from vid

--- WebApiFundamentals/Models/PointOfInterestForCreationDto.cs
+++ WebApiFundamentals/Models/PointOfInterestForCreationDto.cs
@@ -1,9 +1,13 @@
-﻿namespace WebApiFundamentals.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiFundamentals.Models
 {
     public class PointOfInterestForCreationDto
     {
-
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
