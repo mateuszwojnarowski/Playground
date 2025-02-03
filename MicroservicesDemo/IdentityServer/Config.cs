68c7c58d62dad3ec0c441e899b66f3db68e83f7a
Mateusz Wojnarowski <wojnarowski.mateusz@gmail.com>
2025-02-03 21:48:10 +0100

    Added IDS to microservices solution

--- MicroservicesDemo/IdentityServer/Config.cs
+++ MicroservicesDemo/IdentityServer/Config.cs
@@ -1,19 +1,49 @@
 ﻿using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
+
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
-            new IdentityResources.OpenId()
-        };
+    [
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile()
+    ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-            { };
+    [
+        new(name: "order.edit", displayName: "Create orders"),
+        new(name: "order.view", displayName: "View Orders"),
+        new(name: "product.edit", displayName: "Manage Products"),
+        new(name: "product.view", displayName: "View Products")
+    ];
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
-            { };
+    [
+        // better to use one client and have rights to restrict the usage of api calls
+        // but for the sake of this exercise it is better to have two clients
+        new()
+        {
+            ClientId = "client",
+            Description = "Client for admin",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            RedirectUris = { "http://127.0.0.1" },
+
+            AllowedGrantTypes = GrantTypes.Code,
+
+            AllowedScopes = { "order.edit", "order.view", "product.edit", "product.view" },
+            AlwaysIncludeUserClaimsInIdToken = false
+        },
+        new()
+        {
+            ClientId = "client2",
+            Description = "Client for user",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedGrantTypes = GrantTypes.Code,
+
+            RedirectUris = { "http://127.0.0.1" },
+            AllowedScopes = { "order.edit, product.edit" },
+            AlwaysIncludeUserClaimsInIdToken = false
+        }
+    ];
 }
--- MicroservicesDemo/IdentityServer/HostingExtensions.cs
+++ MicroservicesDemo/IdentityServer/HostingExtensions.cs
@@ -1,21 +1,31 @@
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace IdentityServer;
+
 internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
-        // uncomment if you want to add a UI
-        //builder.Services.AddRazorPages();
+        builder.Services.AddRazorPages();
 
-        builder.Services.AddIdentityServer(options =>
+        var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddIdentityServer()
+            .AddConfigurationStore(options =>
             {
-                // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
-                options.EmitStaticAudienceClaim = true;
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
             })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients);
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddTestUsers(TestUsers.Users);
 
         return builder.Build();
     }
@@ -29,16 +39,54 @@ internal static class HostingExtensions
             app.UseDeveloperExceptionPage();
         }
 
-        // uncomment if you want to add a UI
-        //app.UseStaticFiles();
-        //app.UseRouting();
+        InitializeDatabase(app);
+
+        app.UseStaticFiles();
+        app.UseRouting();
 
         app.UseIdentityServer();
 
         // uncomment if you want to add a UI
-        //app.UseAuthorization();
-        //app.MapRazorPages().RequireAuthorization();
+        app.UseAuthorization();
+        app.MapRazorPages().RequireAuthorization();
 
         return app;
+    }
+
+    private static void InitializeDatabase(IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope();
+        serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+        context.Database.Migrate();
+
+        // uncomment if creating the db for the first time
+        if (!context.Clients.Any())
+        {
+            foreach (var client in Config.Clients)
+            {
+                context.Clients.Add(client.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.IdentityResources.Any())
+        {
+            foreach (var resource in Config.IdentityResources)
+            {
+                context.IdentityResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.ApiScopes.Any())
+        {
+            foreach (var resource in Config.ApiScopes)
+            {
+                context.ApiScopes.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
     }
 }
