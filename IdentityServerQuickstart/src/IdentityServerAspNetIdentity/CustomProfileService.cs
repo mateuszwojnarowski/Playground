19f212aa1dd717e759c448f6cc6ca89600b584cd
Mateusz Wojnarowski <wojnarowski.mateusz@gmail.com>
2025-02-05 16:03:14 +0100

    scribbled some extras for experimenting

---
+++ IdentityServerQuickstart/src/IdentityServerAspNetIdentity/CustomProfileService.cs
@@ -0,0 +1,29 @@
+﻿using Duende.IdentityServer.AspNetIdentity;
+using Duende.IdentityServer.Models;
+using IdentityServerAspNetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class CustomProfileService : ProfileService<ApplicationUser>
+    {
+        public CustomProfileService(UserManager<ApplicationUser> userManager, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory) : base(userManager, claimsFactory)
+        {
+        }
+
+        protected override async Task GetProfileDataAsync(ProfileDataRequestContext context, ApplicationUser user)
+        {
+            var principal = await GetUserClaimsAsync(user);
+            var id = (ClaimsIdentity)principal.Identity;
+            if (!string.IsNullOrEmpty(user.FavouriteColour))
+            {
+                id.AddClaim(new Claim("favourite_colour", user.FavouriteColour));
+            }
+
+            //context.AddRequestedClaims(principal.Claims);
+
+            context.IssuedClaims.AddRange(id.Claims);
+        }
+    }
+}
